954dddb9a5a48569fd693a194f5a5ec29e387b7e
Juan Pablo Gonzalez Alvarez <jugonz31@eafit.edu.co>
2019-12-17 17:04:36 -0500

    Updates

---
+++ precache-manifest.7361c6f5aaef5f2fb2c5ef34f4e4a4e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04569cdbe1fe23e98df65d16b5d75b88",
+    "url": "/rick-and-morty-app/index.html"
+  },
+  {
+    "revision": "663981420ff2ccfaa212",
+    "url": "/rick-and-morty-app/static/css/2.728506fe.chunk.css"
+  },
+  {
+    "revision": "8deeb8e84adef049d37f",
+    "url": "/rick-and-morty-app/static/css/main.b612c712.chunk.css"
+  },
+  {
+    "revision": "663981420ff2ccfaa212",
+    "url": "/rick-and-morty-app/static/js/2.af7990c6.chunk.js"
+  },
+  {
+    "revision": "981c245032ea4913502026cc015811df",
+    "url": "/rick-and-morty-app/static/js/2.af7990c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8deeb8e84adef049d37f",
+    "url": "/rick-and-morty-app/static/js/main.b0c14e22.chunk.js"
+  },
+  {
+    "revision": "d4c7210c46e4282b931f",
+    "url": "/rick-and-morty-app/static/js/runtime-main.3a3b8825.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/rick-and-morty-app/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/rick-and-morty-app/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/rick-and-morty-app/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/rick-and-morty-app/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/rick-and-morty-app/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/rick-and-morty-app/static/media/icons-20.d1f0f6c1.ttf"
+  },
+  {
+    "revision": "dc06f50fe90b0e03753bb60e36b935ce",
+    "url": "/rick-and-morty-app/static/media/rick-and-morty-logo.dc06f50f.png"
+  }
+]);

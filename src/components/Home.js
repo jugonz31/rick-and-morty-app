ef16823e0e0fff2d479493928eaaf1a08354df31
Juan Pablo Gonzalez Alvarez <jugonz31@eafit.edu.co>
2019-12-17 17:03:56 -0500

    Adding some changes to the home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import AwsSliderStyles from 'react-awesome-slider/src/styles';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import logo from '../assets/rick-and-morty-logo.png'
 
 export default function Home() {
-  return (
-    <div className="home-container">
-        <h1>Welcome to...</h1>
-        <img src={logo}></img>
-        <AwesomeSlider cssModule={AwsSliderStyles}>
-            <div data-src=""/>
-            <div data-src=""/>
-            <div data-src=""/>
-        </AwesomeSlider>
-    </div>
-  );
+    const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+    return (
+        <div className="home-container">
+            <h1>Welcome to...</h1>
+            <img src={logo}></img>
+            <AutoplaySlider
+                cssModule={AwsSliderStyles}
+                play={true}
+                cancelOnInteraction={false}
+                interval={5000}>
+                <div data-src="" />
+                <div data-src="" />
+                <div data-src="" />
+        </AutoplaySlider>
+        </div>
+    );
 }
